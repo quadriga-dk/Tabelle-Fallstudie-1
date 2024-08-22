641afb65d34fedcd5b01415affd84ba2845430a4
Hannes Schnaitter <git@schnaitter.info>
2024-08-22 09:57:41 +0200

    Add: QUADRIGA Farbdefinitionen

---
+++ quadriga_config/colors.py
@@ -0,0 +1,7 @@
+"""This submodule contains all QUADRIGA color presets for the various libraries used."""
+
+jupyterquiz = {
+    "--jq-multiple-choice-bg": "#00305e",
+    "--jq-many-choice-bg": "#00305e",
+    "--jq-numeric-bg": "#00305e",
+}
